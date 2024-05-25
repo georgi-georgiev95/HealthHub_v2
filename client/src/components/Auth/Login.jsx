1d852da4467878ffb6f66348dc8c4cd2b9cfffe1
Georgi957 <gesh007@abv.bg>
2024-05-25 13:53:29 +0300

    Implemented the validators

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -3,16 +3,22 @@ import { useState } from "react";
 import { firebaseAuth } from "../../config/firebase";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../contexts/UserContext";
+import { validateEmail, validatePassword } from "../../utils/authInputValidator";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState({ email: "", password: "" });
 
   const navigate = useNavigate();
   const { setUser } = useUser();
 
   const submitHandler = async (e) => {
     e.preventDefault();
+
+    if (!email || !password) {
+      return;
+    }
 
     try {
       const userCredential = await firebaseAuth.login(email, password);
@@ -21,35 +27,49 @@ const Login = () => {
       setUser({ userId: user.uid, email: user.email });
       navigate("/catalog");
     } catch (error) {
-      console.log(error);
+      setError({ ...error, email: "Invalid email or password!" });
+      setPassword("");
     }
   };
+
+
 
   return (
     <form className={styles.authForm} onSubmit={submitHandler}>
       <div>
         <label htmlFor="email">Email:</label>
         <input
-          className={styles.field}
+          className={error.email ? styles.fieldError : styles.field}
           type="email"
           id="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          onBlur={(e) => validateEmail(e, error, setError)}
         />
       </div>
+      {error.email && <p className={styles.error}>{error.email}</p>}
       <div>
         <label htmlFor="password">Password:</label>
         <input
-          className={styles.field}
+          className={error.password ? styles.fieldError : styles.field}
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          onBlur={(e) => validatePassword(e, error, setError)}
         />
       </div>
+      {error.password && <p className={styles.error}>{error.password}</p>}
       <div className={styles.buttons}>
         <button className={styles.button} type="submit">
           Login
+        </button>
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => navigate("/users/register")}
+        >
+          Not an user ?
         </button>
       </div>
     </form>
--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -3,17 +3,23 @@ import { useState } from "react";
 import { firebaseAuth } from "../../config/firebase";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../contexts/UserContext";
+import { validateEmail, validatePassword, validateRePassword } from "../../utils/authInputValidator";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rePassword, setRePassword] = useState("");
+  const [error, setError] = useState({ email: "", password: "", rePassword: "" });
 
   const navigate = useNavigate();
   const { setUser } = useUser();
 
   const submitHandler = async (e) => {
     e.preventDefault();
+
+    if (!email || !password) {
+      return;
+    }
 
     if (password !== rePassword) {
       console.log("Passwords do not match");
@@ -28,7 +34,9 @@ const Register = () => {
 
       navigate("/catalog");
     } catch (error) {
-      console.log(error);
+      setError({ ...error, email: "Email already registered!" });
+      setPassword("");
+      setRePassword("");
     }
   };
 
@@ -37,36 +45,49 @@ const Register = () => {
       <div>
         <label htmlFor="email">Email:</label>
         <input
-          className={styles.field}
+          className={error.email ? styles.fieldError : styles.field}
           type="email"
           id="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          onBlur={(e) => validateEmail(e, error, setError)}
         />
       </div>
+      {error.email && <p className={styles.error}>{error.email}</p>}
       <div>
         <label htmlFor="password">Password:</label>
         <input
-          className={styles.field}
+          className={error.email ? styles.fieldError : styles.field}
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          onBlur={(e) => validatePassword(e, error, setError)}
         />
       </div>
+      {error.password && <p className={styles.error}>{error.password}</p>}
       <div>
         <label htmlFor="rePassword">Repeat Password:</label>
         <input
-          className={styles.field}
+          className={error.email ? styles.fieldError : styles.field}
           type="password"
           id="rePassword"
           value={rePassword}
           onChange={(e) => setRePassword(e.target.value)}
+          onBlur={(e) => validateRePassword(e, password, error, setError)}
         />
       </div>
+      {error.rePassword && <p className={styles.error}>{error.rePassword}</p>}
       <div className={styles.buttons}>
         <button className={styles.button} type="submit">
           Register
+        </button>
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => navigate("/users/login")}
+        >
+          Already an user ?
         </button>
       </div>
     </form>
