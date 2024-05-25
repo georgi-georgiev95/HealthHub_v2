872fd300203981e30bdcec9100afd6e7eae4b0d8
Georgi957 <gesh007@abv.bg>
2024-05-25 12:30:39 +0300

    Created config for firebase

---
+++ client/src/config/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqmVfoEIdv7WdIQJBRJ_nBODHWeUQZ7Z4",
+    authDomain: "healthhub-ddadd.firebaseapp.com",
+    projectId: "healthhub-ddadd",
+    storageBucket: "healthhub-ddadd.appspot.com",
+    messagingSenderId: "649780624951",
+    appId: "1:649780624951:web:fdb16d327a06ba39b589b2",
+    measurementId: "G-47M3SK3XC9"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const firebaseAuth = {
+    register: (email, password) => createUserWithEmailAndPassword(auth, email, password),
+    login: (email, password) => signInWithEmailAndPassword(auth, email, password),
+    logout: () => signOut(auth),
+    onAuthStateChanged: (callback) => onAuthStateChanged(auth, callback),
+    currentUser: () => auth.currentUser
+};
+
+export { firebaseAuth };
