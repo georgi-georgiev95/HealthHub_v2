7708bed6712d950510da6fb9de400406d2e6f8d5
Georgi957 <gesh007@abv.bg>
2024-05-25 12:40:20 +0300

    Successfully logged in a registered user

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,35 +1,57 @@
-import styles from './AuthForms.module.css';
-import { useState } from 'react';
+import styles from "./AuthForms.module.css";
+import { useState } from "react";
+import { firebaseAuth } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
-        console.log(email, password);
+    try {
+      const userCredential = await firebaseAuth.login(email, password);
+      const user = userCredential.user;
 
-        setEmail('');
-        setPassword('');
+      console.log(user);
+      navigate("/catalog");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-      <form className={styles.authForm} onSubmit={submitHandler}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input className={styles.field} type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input className={styles.field} type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <div className={styles.buttons}>
-          <button className={styles.button} type="submit">Login</button>
-        </div>
-      </form>
-    );
+  return (
+    <form className={styles.authForm} onSubmit={submitHandler}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          className={styles.field}
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          className={styles.field}
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.button} type="submit">
+          Login
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
