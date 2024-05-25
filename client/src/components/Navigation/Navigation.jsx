2b4e642d593b6a69c0e9205d7b8e29b4e704f608
Georgi957 <gesh007@abv.bg>
2024-05-25 12:10:57 +0300

    Replaced a tags with Link

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,15 +1,16 @@
 import styles from './Navigation.module.css';
+import {Link} from 'react-router-dom'
 
 const Navigation = () => {
     return (
-        <nav className={styles.siteNavigation}>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Catalog</a></li>
-                <li><a href="#">Login</a></li>
-            </ul>
-        </nav>
-    )
+      <nav className={styles.siteNavigation}>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="catalog">Catalog</Link>
+          <Link to="users/login">Login</Link>
+        </ul>
+      </nav>
+    );
 };
 
 export default Navigation;
