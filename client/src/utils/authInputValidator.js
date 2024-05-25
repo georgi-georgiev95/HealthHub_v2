8f6fe7ea1e89100bc0addbaad66b540461330140
Georgi957 <gesh007@abv.bg>
2024-05-25 13:53:19 +0300

    Created validators for the auth forms

---
+++ client/src/utils/authInputValidator.js
@@ -0,0 +1,57 @@
+const validateEmail = (e, error, setError) => {
+    if (!e.target.value) {
+        setError({ ...error, email: `Email is required!` });
+        return;
+    }
+
+    const email = e.target.value.trim();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(String(email).toLowerCase());
+
+    if (!isValid) {
+        setError({ ...error, email: `Invalid email!` });
+    } else {
+        setError({ ...error, email: '' });
+    }
+};
+
+const validatePassword = (e, error, setError) => {
+    if (!e.target.value) {
+        setError({ ...error, password: `Password is required!` });
+        return;
+    }
+
+    if(e.target.value.length < 6) {
+        setError({ ...error, password: `Password must be at least 6 characters long!` });
+        return;
+    }
+
+    if(e.target.value.length > 20) {
+        setError({ ...error, password: `Password must be at most 20 characters long!` });
+        return;
+    }
+
+    setError({ ...error, password: '' });
+};
+
+const validateRePassword = (e, password, error, setError) => {
+    if (!e.target.value) {
+        setError({ ...error, rePassword: `Repeat password is required!` });
+        return;
+    }
+
+    if(password && e.target.value !== password) {
+        setError({ ...error, rePassword: `Passwords do not match!` });
+        return;
+    }
+
+    setError({ ...error, rePassword: '' });
+};
+
+
+export {
+    validateEmail,
+    validatePassword,
+    validateRePassword,
+}
