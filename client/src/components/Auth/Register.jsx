af140796648f30d8a6e8938445757b5ae003798e
Georgi957 <gesh007@abv.bg>
2024-05-25 12:37:41 +0300

    Successfully registered a user with firebase

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,60 +1,73 @@
-import styles from './AuthForms.module.css';
-import { useState } from 'react';
+import styles from "./AuthForms.module.css";
+import { useState } from "react";
+import { firebaseAuth } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-   const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [rePassword, setRePassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
 
-   const submitHandler = (e) => {
-     e.preventDefault();
+  const navigate = useNavigate();
 
-     console.log(email, password);
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
-     setEmail("");
-       setPassword("");
-       setRePassword("");
-   };
+    if (password !== rePassword) {
+      console.log("Passwords do not match");
+      return;
+    }
 
-   return (
-     <form className={styles.authForm} onSubmit={submitHandler}>
-       <div>
-         <label htmlFor="email">Email:</label>
-         <input
-           className={styles.field}
-           type="email"
-           id="email"
-           value={email}
-           onChange={(e) => setEmail(e.target.value)}
-         />
-       </div>
-       <div>
-         <label htmlFor="password">Password:</label>
-         <input
-           className={styles.field}
-           type="password"
-           id="password"
-           value={password}
-           onChange={(e) => setPassword(e.target.value)}
-         />
-       </div>
-       <div>
-         <label htmlFor="rePassword">Repeat Password:</label>
-         <input
-           className={styles.field}
-           type="password"
-           id="rePassword"
-           value={rePassword}
-           onChange={(e) => setRePassword(e.target.value)}
-         />
-       </div>
-       <div className={styles.buttons}>
-         <button className={styles.button} type="submit">
-           Register
-         </button>
-       </div>
-     </form>
-   );
+    try {
+      const userCredential = await firebaseAuth.register(email, password);
+      const user = userCredential.user;
+      console.log(user);
+
+      navigate("/catalog");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form className={styles.authForm} onSubmit={submitHandler}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          className={styles.field}
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          className={styles.field}
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="rePassword">Repeat Password:</label>
+        <input
+          className={styles.field}
+          type="password"
+          id="rePassword"
+          value={rePassword}
+          onChange={(e) => setRePassword(e.target.value)}
+        />
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.button} type="submit">
+          Register
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Register;
