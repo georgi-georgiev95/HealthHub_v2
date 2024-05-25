1432a2dda3989d81210ba648d307e4a64a1fb8cb
Georgi957 <gesh007@abv.bg>
2024-05-25 12:29:24 +0300

    Created register form

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,7 +1,60 @@
+import styles from './AuthForms.module.css';
+import { useState } from 'react';
+
 const Register = () => {
-    return (
-        <h1>Register Page</h1>
-    );
+   const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [rePassword, setRePassword] = useState("");
+
+   const submitHandler = (e) => {
+     e.preventDefault();
+
+     console.log(email, password);
+
+     setEmail("");
+       setPassword("");
+       setRePassword("");
+   };
+
+   return (
+     <form className={styles.authForm} onSubmit={submitHandler}>
+       <div>
+         <label htmlFor="email">Email:</label>
+         <input
+           className={styles.field}
+           type="email"
+           id="email"
+           value={email}
+           onChange={(e) => setEmail(e.target.value)}
+         />
+       </div>
+       <div>
+         <label htmlFor="password">Password:</label>
+         <input
+           className={styles.field}
+           type="password"
+           id="password"
+           value={password}
+           onChange={(e) => setPassword(e.target.value)}
+         />
+       </div>
+       <div>
+         <label htmlFor="rePassword">Repeat Password:</label>
+         <input
+           className={styles.field}
+           type="password"
+           id="rePassword"
+           value={rePassword}
+           onChange={(e) => setRePassword(e.target.value)}
+         />
+       </div>
+       <div className={styles.buttons}>
+         <button className={styles.button} type="submit">
+           Register
+         </button>
+       </div>
+     </form>
+   );
 };
 
 export default Register;
