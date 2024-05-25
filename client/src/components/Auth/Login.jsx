aa4d03d2a0ba821fb7bc6ad11f9e0d5204e238fe
Georgi957 <gesh007@abv.bg>
2024-05-25 12:27:08 +0300

    Created login form

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,6 +1,34 @@
+import styles from './AuthForms.module.css';
+import { useState } from 'react';
+
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log(email, password);
+
+        setEmail('');
+        setPassword('');
+    }
+
     return (
-        <h1>Login Page</h1>
+      <form className={styles.authForm} onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input className={styles.field} type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input className={styles.field} type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.button} type="submit">Login</button>
+        </div>
+      </form>
     );
 };
 
