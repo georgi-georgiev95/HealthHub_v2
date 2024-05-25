bb64ea62507e1de041e384ec03c7e3a4b55c4264
Georgi957 <gesh007@abv.bg>
2024-05-25 12:08:56 +0300

    Added router in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
 import Navigation from './components/Navigation/Navigation'
+import Home from './components/Home/Home'
+import Catalog from './components/Catalog/Catalog'
+import Login from './components/Auth/Login'
 
 function App() {
 
   return (
     <>
       <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Catalog />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
