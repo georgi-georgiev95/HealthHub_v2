a74cdc0723045694503dd2251fe5b4d6f2576d82
Georgi957 <gesh007@abv.bg>
2024-05-26 14:16:04 +0300

    Changed navigation layout

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,11 +1,11 @@
-import styles from './Navigation.module.css'
-import { firebaseAuth } from '../../config/firebase'
-import {useUser} from '../../contexts/UserContext'
-import {Link, useNavigate} from 'react-router-dom'
+import styles from "./Navigation.module.css";
+import { firebaseAuth } from "../../config/firebase";
+import { useUser } from "../../contexts/UserContext";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navigation = () => {
   const navigate = useNavigate();
-  const {user, setUser} = useUser();
+  const { user, setUser } = useUser();
 
   const logoutHandler = async () => {
     await firebaseAuth.logout();
@@ -14,28 +14,30 @@ const Navigation = () => {
   };
 
   return (
-    <>
-      {user.username && (
-        <p className={styles.username}>Welcome, {user.username}!</p>
-      )}
-      <nav className={styles.siteNavigation}>
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="catalog">Catalog</Link>
-          {user.username && <Link to="create">Add</Link>}
-        </ul>
-        <ul>
-          {user.email ? (
-            <Link onClick={logoutHandler}>Logout</Link>
-          ) : (
-            <>
-              <Link to="users/login">Login</Link>
-              <Link to="users/register">Register</Link>
-            </>
-          )}
-        </ul>
-      </nav>
-    </>
+    <div className={styles.navWrapper}>
+      <div className={styles.navContent}>
+        {user.username && (
+          <p className={styles.username}>Welcome, {user.username}!</p>
+        )}
+        <nav className={styles.siteNavigation}>
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="catalog">Catalog</Link>
+            {user.username && <Link to="create">Add</Link>}
+          </ul>
+          <ul>
+            {user.email ? (
+              <Link onClick={logoutHandler}>Logout</Link>
+            ) : (
+              <>
+                <Link to="users/login">Login</Link>
+                <Link to="users/register">Register</Link>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
