3fef53ff3bd628c59ec37a6fa6a100a0edf02dc5
Georgi957 <gesh007@abv.bg>
2024-05-26 14:17:51 +0300

    Added new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import { useUser } from './contexts/UserContext'
 import CreateRecipe from './components/CreateForms/RecipeCreate'
 import CreateHomeView from './components/CreateForms/CreateHomeView'
 import CreateWorkout from './components/CreateForms/WorkoutCreate'
+import RecipeCatalog from './components/RecipeCatalog/RecipeCatalog'
+import WorkoutCatalog from './components/WorkoutCatalog/WorkoutCatalog'
 
 function App() {
   const { loading } = useUser();
@@ -17,20 +19,24 @@ function App() {
   if(loading) return <Loader />
 
   return (
-    <div className="site">
+    <>
       <Navigation />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="create" element={<CreateHomeView />} />
-          <Route path="create/recipe" element={<CreateRecipe />} />
-          <Route path="create/workout" element={<CreateWorkout />} />
-          <Route path="users/login" element={<Login />} />
-          <Route path="users/register" element={<Register />} />
-        </Routes>
+      <div className="site">
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route path="catalog/recipes" element={<RecipeCatalog />} />
+            <Route path="catalog/workouts" element={<WorkoutCatalog/>} />
+            <Route path="create" element={<CreateHomeView />} />
+            <Route path="create/recipe" element={<CreateRecipe />} />
+            <Route path="create/workout" element={<CreateWorkout />} />
+            <Route path="users/login" element={<Login />} />
+            <Route path="users/register" element={<Register />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
