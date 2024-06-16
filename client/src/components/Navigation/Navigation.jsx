318a159a09cf1095d31a380b2e42d632f1cda90e
Georgi957 <gesh007@abv.bg>
2024-06-16 18:46:58 +0300

    Added bootstrap

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -3,6 +3,12 @@ import { firebaseAuth } from "../../config/firebase";
 import { useUser } from "../../contexts/UserContext";
 import { Link, useNavigate } from "react-router-dom";
 
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
 const Navigation = () => {
   const navigate = useNavigate();
   const { user, setUser } = useUser();
@@ -14,30 +20,58 @@ const Navigation = () => {
   };
 
   return (
-    <div className={styles.navWrapper}>
-      <div className={styles.navContent}>
-        {user.username && (
-          <p className={styles.username}>Welcome, {user.username}!</p>
-        )}
-        <nav className={styles.siteNavigation}>
-          <ul>
-            <Link to="/">Home</Link>
-            <Link to="catalog">Catalog</Link>
-            {user.username && <Link to="create">Add</Link>}
-          </ul>
-          <ul>
-            {user.email ? (
-              <Link onClick={logoutHandler}>Logout</Link>
-            ) : (
-              <>
-                <Link to="users/login">Login</Link>
-                <Link to="users/register">Register</Link>
-              </>
-            )}
-          </ul>
-        </nav>
+    <>
+      <div className={styles.navWrapper}>
+        <div className={styles.navContent}>
+          {user.username && (
+            <p className={styles.username}>Welcome, {user.username}!</p>
+          )}
+          <nav className={styles.siteNavigation}>
+            <ul>
+              <Link to="/">Home</Link>
+              <Link to="catalog">Catalog</Link>
+              {user.username && <Link to="create">Add</Link>}
+            </ul>
+            <ul>
+              {user.email ? (
+                <Link onClick={logoutHandler}>Logout</Link>
+              ) : (
+                <>
+                  <Link to="users/login">Login</Link>
+                  <Link to="users/register">Register</Link>
+                </>
+              )}
+            </ul>
+          </nav>
+        </div>
       </div>
-    </div>
+
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
