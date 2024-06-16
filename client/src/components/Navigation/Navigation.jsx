204b462013b26fdb4fb15ce53d55b385a4e92e1e
Georgi957 <gesh007@abv.bg>
2024-06-16 20:14:46 +0300

    Remove boostrap nav

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -3,12 +3,6 @@ import { firebaseAuth } from "../../config/firebase";
 import { useUser } from "../../contexts/UserContext";
 import { Link, useNavigate } from "react-router-dom";
 
-
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
 const Navigation = () => {
   const navigate = useNavigate();
   const { user, setUser } = useUser();
@@ -20,7 +14,6 @@ const Navigation = () => {
   };
 
   return (
-    <>
       <div className={styles.navWrapper}>
         <div className={styles.navContent}>
           {user.username && (
@@ -45,33 +38,6 @@ const Navigation = () => {
           </nav>
         </div>
       </div>
-
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
   );
 };
 
