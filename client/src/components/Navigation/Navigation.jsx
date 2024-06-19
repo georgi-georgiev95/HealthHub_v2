1c74d571e63868502bf70f4cae78b109db7013f7
Georgi Georgiev <122177357+georgi-georgiev95@users.noreply.github.com>
2024-06-19 12:55:52 +0300

    Added mobile burger nav

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -2,16 +2,22 @@ import styles from "./Navigation.module.css";
 import { firebaseAuth } from "../../config/firebase";
 import { useUser } from "../../contexts/UserContext";
 import { Link, useNavigate } from "react-router-dom";
+import {useState, useEffect} from "react";
 
 const Navigation = () => {
   const navigate = useNavigate();
   const { user, setUser } = useUser();
+  const [mobileNavStatus, setMobileNavStatus] = useState(false);
 
   const logoutHandler = async () => {
     await firebaseAuth.logout();
     setUser({});
     navigate("/");
   };
+
+  const expandNav = () => {
+    setMobileNavStatus(!mobileNavStatus);
+  }
 
   return (
       <div className={styles.navWrapper}>
@@ -20,8 +26,8 @@ const Navigation = () => {
             <p className={styles.username}>Welcome, {user.username}!</p>
           )}
           <nav className={styles.siteNavigation}>
-            <ul>
               <Link to="/"><p className={styles.logo}></p></Link>
+            <ul>
               <Link to="catalog">Catalog</Link>
               {user.username && <Link to="create">Add</Link>}
             </ul>
@@ -30,12 +36,26 @@ const Navigation = () => {
                 <Link onClick={logoutHandler}>Logout</Link>
               ) : (
                 <>
-                  <Link to="users/login">Login</Link>
-                  <Link to="users/register">Register</Link>
+                  <Link to="users/login">Sign in</Link>
+                  <Link to="users/register" className={styles.signUp}>Sign up</Link>
                 </>
               )}
             </ul>
+            {mobileNavStatus ? <i onClick={expandNav} className="fa-solid fa-x"></i> : <i onClick={expandNav} className="fa-solid fa-bars"></i> }         
           </nav>
+          {mobileNavStatus && 
+            <ul className={styles.dropDown}>
+              <Link to="catalog">Catalog</Link>
+              {user.username && <Link to="create">Add</Link>}
+              {user.email ? (
+                <Link onClick={logoutHandler}>Logout</Link>
+              ) : (
+                <>
+                  <Link to="users/login">Sign in</Link>
+                  <Link to="users/register" className={styles.signUp}>Sign up</Link>
+                </>
+              )}
+            </ul>}
         </div>
       </div>
   );
