0cef3568af325a14832e1d85165bbcede8f4e01d
Georgi957 <gesh007@abv.bg>
2024-07-01 13:28:22 +0300

    Adjust the renamed components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,9 @@ import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import Loader from './components/Shared/Loader'
 import { useUser } from './contexts/UserContext'
-import CreateRecipe from './components/CreateForms/RecipeCreate'
-import CreateHomeView from './components/CreateForms/CreateHomeView'
-import CreateWorkout from './components/CreateForms/WorkoutCreate'
+import CreateRecipe from './components/CreateForms/CreateRecipeForm/RecipeCreate'
+import PreCreateFormView from "./components/CreateForms/PreCreateFormView";
+import CreateWorkout from './components/CreateForms/CreateWorkoutForm/WorkoutCreate'
 import RecipeCatalog from './components/RecipeCatalog/RecipeCatalog'
 import WorkoutCatalog from './components/WorkoutCatalog/WorkoutCatalog'
 import RecipeDetails from './components/RecipeDetails/RecipeDetails'
@@ -29,8 +29,8 @@ function App() {
             <Route path="catalog" element={<Catalog />} />
             <Route path="catalog/recipes" element={<RecipeCatalog />} />
             <Route path="catalog/recipes/:id" element={<RecipeDetails />} />
-            <Route path="catalog/workouts" element={<WorkoutCatalog/>} />
-            <Route path="create" element={<CreateHomeView />} />
+            <Route path="catalog/workouts" element={<WorkoutCatalog />} />
+            <Route path="create" element={<PreCreateFormView />} />
             <Route path="create/recipe" element={<CreateRecipe />} />
             <Route path="create/workout" element={<CreateWorkout />} />
             <Route path="users/login" element={<Login />} />
