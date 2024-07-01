45a51083b10001f936345f29dda4994e84666386
Georgi957 <gesh007@abv.bg>
2024-07-01 17:54:38 +0300

    Burger menu color change

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -36,53 +36,93 @@ const Navigation = () => {
   }
 
   return (
-      <div className={styles.navWrapper}>
-        <div className={styles.navContent}>
-          <nav className={styles.siteNavigation}>
-              <Link onClick={collapseNav} to="/"><p className={styles.logo}></p></Link>
-            <ul>
-              <Link onClick={collapseNav} to="catalog">Catalog</Link>
-              {user.username && <Link onClick={collapseNav} to="create">Add</Link>}
-            </ul>
-            <ul className={styles.profileDropdown}>
-              {user.email ? (
-                <>
-                  {user.username && (
-                    <>
-                    <button onClick={expandProfile}><i className="fa-regular fa-user"></i></button>
-                    </>
-                  )}
-                </>
-              ) : (
-                <>
-                  <Link to="users/login">Sign in</Link>
-                  <Link to="users/register" className={styles.signUp}>Sign up</Link>
-                </>
-              )}
-            </ul>
-            {mobileNavStatus ? <i onClick={expandNav} className="fa-solid fa-x"></i> : <i onClick={expandNav} className="fa-solid fa-bars"></i> }         
-          </nav>
-          {mobileNavStatus && 
-            <ul className={styles.dropDown}>
-              <Link onClick={collapseNav} to="catalog">Catalog</Link>
-              {user.username && <Link onClick={collapseNav} to="create">Add</Link>}
-              {user.email ? (
-                  <Link onClick={handleLogout}>Logout</Link>
-              ) : (
-                <>
-                  <Link onClick={collapseNav} to="users/login">Sign in</Link>
-                  <Link onClick={collapseNav} to="users/register" className={styles.signUp}>Sign up</Link>
-                </>
-              )}
-            </ul>}
-            {profileDetailsVisibility && 
-            <ul className={styles.profileMenu}>
-                    <p className={styles.username}>Welcome, {user.username}!</p>
-                    <Link>Profile</Link>
-                    <Link onClick={handleLogout}>Logout</Link>
-            </ul>}
-        </div>
+    <div className={styles.navWrapper}>
+      <div className={styles.navContent}>
+        <nav className={styles.siteNavigation}>
+          <Link onClick={collapseNav} to="/">
+            <p className={styles.logo}></p>
+          </Link>
+          <ul>
+            <Link onClick={collapseNav} to="catalog">
+              Catalog
+            </Link>
+            {user.username && (
+              <Link onClick={collapseNav} to="create">
+                Add
+              </Link>
+            )}
+          </ul>
+          <ul className={styles.profileDropdown}>
+            {user.email ? (
+              <>
+                {user.username && (
+                  <>
+                    <button onClick={expandProfile}>
+                      <i className="fa-regular fa-user"></i>
+                    </button>
+                  </>
+                )}
+              </>
+            ) : (
+              <>
+                <Link to="users/login">Sign in</Link>
+                <Link to="users/register" className={styles.signUp}>
+                  Sign up
+                </Link>
+              </>
+            )}
+          </ul>
+          {mobileNavStatus ? (
+            <i
+              onClick={expandNav}
+              className="fa-solid fa-x"
+              style={{ color: "#ffffff" }}
+            ></i>
+          ) : (
+            <i
+              onClick={expandNav}
+              className="fa-solid fa-bars"
+              style={{ color: "#ffffff" }}
+            ></i>
+          )}
+        </nav>
+        {mobileNavStatus && (
+          <ul className={styles.dropDown}>
+            <Link onClick={collapseNav} to="catalog">
+              Catalog
+            </Link>
+            {user.username && (
+              <Link onClick={collapseNav} to="create">
+                Add
+              </Link>
+            )}
+            {user.email ? (
+              <Link onClick={handleLogout}>Logout</Link>
+            ) : (
+              <>
+                <Link onClick={collapseNav} to="users/login">
+                  Sign in
+                </Link>
+                <Link
+                  onClick={collapseNav}
+                  to="users/register"
+                  className={styles.signUp}
+                >
+                  Sign up
+                </Link>
+              </>
+            )}
+          </ul>
+        )}
+        {profileDetailsVisibility && (
+          <ul className={styles.profileMenu}>
+            <p className={styles.username}>Welcome, {user.username}!</p>
+            <Link>Profile</Link>
+            <Link onClick={handleLogout}>Logout</Link>
+          </ul>
+        )}
       </div>
+    </div>
   );
 };
 
