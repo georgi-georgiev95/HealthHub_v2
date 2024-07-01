09d55214f2f21aff762b07382253a15ca3a5859e
Georgi957 <gesh007@abv.bg>
2024-07-01 17:05:27 +0300

    Created Delete Confirmation modal

---
+++ client/src/components/Shared/DeleteModal/DeleteConfirmationModal.jsx
@@ -0,0 +1,31 @@
+import styles from "./DeleteConfirmationModal.module.css";
+import PropTypes from "prop-types";
+
+
+const DeleteConfirmationModal = ({ isOpen, onCancel, onConfirm }) => {
+  return (
+    isOpen && (
+      <div className={styles.modalOverlay}>
+        <div className={styles.modal}>
+          <p>Are you sure you want to delete this recipe?</p>
+          <div className={styles.buttons}>
+            <button className={styles.cancelButton} onClick={onCancel}>
+              Cancel
+            </button>
+            <button className={styles.confirmButton} onClick={onConfirm}>
+              Confirm Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+DeleteConfirmationModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+};
+
+export default DeleteConfirmationModal;
