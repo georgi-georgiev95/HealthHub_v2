39dd4a6b59d5669b156f0737c541ede0963d3622
Georgi957 <gesh007@abv.bg>
2024-07-01 13:27:56 +0300

    Extracted grid card in separate component

--- client/src/components/RecipeCatalog/RecipeCatalog.jsx
+++ client/src/components/RecipeCatalog/RecipeCatalog.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllRecipes } from "../../services/recipeService";
-import { Link } from "react-router-dom";
 import styles from "./RecipeCatalog.module.css";
+import RecipeGridCard from "./RecipeGridCard/RecipeGridCard";
 
 const RecipeCatalog = () => {
   const [recipes, setRecipes] = useState([]);
@@ -23,24 +23,7 @@ const RecipeCatalog = () => {
       </div>
       <div className={styles.container}>
         {recipes.length > 0 && recipes.map((recipe) => (
-          <Link to={`/catalog/recipes/${recipe.id}`} key={recipe.id}>
-          <div className={styles["flip-card"]} key={recipe.id}>
-            <div className={styles["flip-card-inner"]}>
-              <div
-                className={styles["flip-card-front"]}
-                style={{
-                  backgroundImage: `url(${recipe.image})`,
-                  backgroundSize: `cover`,
-                }}
-              ></div>
-              <div className={styles["flip-card-back"]}>
-                <h2>{recipe.title}</h2>
-                <p>Difficulty level: {recipe.difficulty}</p>
-                <p>Added by: {recipe.ownerName}</p>
-              </div>
-            </div>
-            </div>
-          </Link>
+          <RecipeGridCard key={recipe.id} recipeData={recipe} />
         ))}
       </div>
     </>
---
+++ client/src/components/RecipeCatalog/RecipeGridCard/RecipeGridCard.jsx
@@ -0,0 +1,38 @@
+import styles from "../RecipeCatalog.module.css";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const RecipeGridCard = ({ recipeData }) => {
+    return (
+      <Link to={`/catalog/recipes/${recipeData.id}`} key={recipeData.id}>
+        <div className={styles["flip-card"]} key={recipeData.id}>
+          <div className={styles["flip-card-inner"]}>
+            <div
+              className={styles["flip-card-front"]}
+              style={{
+                backgroundImage: `url(${recipeData.image})`,
+                backgroundSize: `cover`,
+              }}
+            ></div>
+            <div className={styles["flip-card-back"]}>
+              <h2>{recipeData.title}</h2>
+              <p>Difficulty level: {recipeData.difficulty}</p>
+              <p>Added by: {recipeData.ownerName}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    );
+};
+
+RecipeGridCard.propTypes = {
+  recipeData: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    difficulty: PropTypes.string.isRequired,
+    ownerName: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default RecipeGridCard;
