95edc8b3b16d7c64a4747c5d9b3d20acdcfd82e4
Georgi957 <gesh007@abv.bg>
2024-07-01 15:01:38 +0300

    Added logic to edit form

--- client/src/components/EditForms/EditRecipe.jsx
+++ client/src/components/EditForms/EditRecipe.jsx
@@ -1,12 +1,139 @@
-import styles from "../CreateForms/EntityForm.module.css"
+import styles from "../CreateForms/EntityForm.module.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getOneRecipe } from "../../services/recipeService";
 
 const EditRecipe = () => {
-    return (
-      <>
-        <h2 className={styles.formTitle}>Edit Recipe</h2>
-        
-      </>
-    );
+  const [recipe, setRecipe] = useState({
+    title: "",
+    image: "",
+    description: "",
+    ingredients: [],
+    instructions: "",
+    difficulty: 1,
+  });
+  const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      await getOneRecipe(id, setRecipe);
+    })();
+  }, [id]);
+
+  const addInputField = () => {
+    if (recipe.ingredients[recipe.ingredients.length - 1] === "") {
+      return;
+    }
+    setRecipe({
+      ...recipe,
+      ingredients: [...recipe.ingredients, ""],
+    });
+  };
+
+  const deleteInputField = (index) => {
+    setRecipe({
+      ...recipe,
+      ingredients: recipe.ingredients.filter((_, i) => i !== index),
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const recipeData = recipe;
+
+    console.log(recipeData);
+  };
+
+  return (
+    <>
+      <h2 className={styles.formTitle}>Edit Recipe</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={recipe.title}
+            onChange={(e) => setRecipe({ ...recipe, title: e.target.value })}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            name="description"
+            id="description"
+            value={recipe.description}
+            onChange={(e) =>
+              setRecipe({ ...recipe, description: e.target.value })
+            }
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="ingredients">Ingredients:</label>
+          {recipe.ingredients.map((ingredient, index) => (
+            <div key={index} className={styles.ingredientGroup}>
+              <input
+                type="text"
+                value={ingredient}
+                onChange={(e) =>
+                  setRecipe({
+                    ...recipe,
+                    ingredients: [
+                      ...recipe.ingredients.slice(0, index),
+                      e.target.value,
+                      ...recipe.ingredients.slice(index + 1),
+                    ],
+                  })
+                }
+              />
+                {recipe.ingredients.length > 1 && <i onClick={() => deleteInputField(index)} className="fa-solid fa-x" ></i>}  
+            </div>
+          ))}
+          <button
+            type="button"
+            className={styles.addButton}
+            onClick={addInputField}
+          >
+            Add Ingredient
+          </button>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="image">ImageURL:</label>
+          <input
+            type="text"
+            name="image"
+            id="image"
+            value={recipe.image}
+            onChange={(e) => setRecipe({ ...recipe, image: e.target.value })}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="difficulty">Difficulty Level:</label>
+          <select
+            name="difficulty"
+            id="difficulty"
+            value={recipe.difficulty}
+            onChange={(e) =>
+              setRecipe({ ...recipe, difficulty: parseInt(e.target.value) })
+            }
+          >
+            {[...Array(10).keys()].map((num) => (
+              <option key={num + 1} value={num + 1}>
+                {num + 1}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.button} type="submit">
+            Save Changes
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default EditRecipe;
