8573a1fd67d50e24d0eeb56bd73ada025e4b3efd
Georgi957 <gesh007@abv.bg>
2024-07-01 13:26:48 +0300

    Renamed pre create component

---
+++ client/src/components/CreateForms/PreCreateFormView.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import styles from './PreCreateFormView.module.css'
+
+const PreCreateFormView = () => {
+    return (
+      <>
+        <h2 className={styles.catalogIntro}>Create your own</h2>
+        <div className={styles.createHomeView}>
+          <Link className={styles.groupLink} to="/create/recipe">
+            <div className={styles.groupLink}>
+              <div className={styles.upperSide}>
+                <img
+                  src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505"
+                  alt=""
+                />
+              </div>
+              <div className={styles.bottomSide}>
+                <h3>Recipes</h3>
+              </div>
+            </div>
+          </Link>
+          <div className={styles.logo}></div>
+          <Link
+            className={`${styles.groupLink} ${styles.reverse}`}
+            to="/create/workout"
+          >
+            <div className={`${styles.groupLink} ${styles.reverse}`}>
+              <div className={styles.bottomSide}>
+                <img
+                  src="https://media.istockphoto.com/id/1391410249/photo/sports-and-gym-activities.jpg?s=612x612&w=0&k=20&c=1S-hAmT-CkRtdYV_hcKi1lZdQkXAN_mCy3ebIXlUEnE="
+                  alt=""
+                />
+              </div>
+              <div className={styles.upperSide}>
+                <h3>Workouts</h3>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </>
+    );
+};
+
+export default PreCreateFormView;
