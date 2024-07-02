464af3a7ac4eda0fa65c32314d9da6f2259e1709
Georgi957 <gesh007@abv.bg>
2024-07-02 17:41:20 +0300

    Created recipe card for the profile component

---
+++ client/src/components/UserProfile/RecipeProfileCard/RecipeProfileCard.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import styles from "./RecipeProfileCard.module.css"
+import { Link } from "react-router-dom";
+
+const RecipeProfileCard = ({ recipeData }) => {
+
+    const difficulty = "⭐".repeat(recipeData.difficulty);
+
+    return (
+        <Link to={`/catalog/recipes/${recipeData.id}`}>
+            <div className={styles.recipeCard}>
+                <img src={recipeData.image} alt="recipe" />
+                <span>{difficulty}</span>
+                <p>{recipeData.title}</p>
+            </div>
+        </Link>
+    );
+};
+
+RecipeProfileCard.propTypes = {
+    recipeData: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        difficulty: PropTypes.number.isRequired,
+        ownerName: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default RecipeProfileCard;
