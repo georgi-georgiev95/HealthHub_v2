29cc54cc4126b4d054f8e9ddabcd4c867ca1a747
Georgi957 <gesh007@abv.bg>
2024-07-02 17:39:02 +0300

    Added user profile component to routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import RecipeCatalog from './components/RecipeCatalog/RecipeCatalog'
 import WorkoutCatalog from './components/WorkoutCatalog/WorkoutCatalog'
 import RecipeDetails from './components/RecipeDetails/RecipeDetails'
 import EditRecipe from './components/EditForms/EditRecipe'
+import UserProfile from './components/UserProfile/UserProfile'
 
 function App() {
   const { loading } = useUser();
@@ -37,6 +38,7 @@ function App() {
             <Route path="create/workout" element={<CreateWorkout />} />
             <Route path="users/login" element={<Login />} />
             <Route path="users/register" element={<Register />} />
+            <Route path="users/profile" element={<UserProfile/>} />
           </Routes>
         </div>
       </div>
