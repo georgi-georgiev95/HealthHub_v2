bbb7bd163c48b90d6291830a18def629190eb0b1
Georgi957 <gesh007@abv.bg>
2024-07-06 22:00:31 +0300

    Added dotenv and extracted api key

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBqmVfoEIdv7WdIQJBRJ_nBODHWeUQZ7Z4",
+    apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
     authDomain: "healthhub-ddadd.firebaseapp.com",
     projectId: "healthhub-ddadd",
     storageBucket: "healthhub-ddadd.appspot.com",
