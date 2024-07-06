42353d17d98534bc939c5e27de99b4d7681ecd02
Georgi957 <gesh007@abv.bg>
2024-07-06 20:35:15 +0300

    Created card for workout in user profile

---
+++ client/src/components/UserProfile/WorkoutProfileCard/WorkoutProfileCard.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import styles from "./WorkoutProfileCard.module.css";
+import { Link } from "react-router-dom";
+import fatLossImage from "../../Catalogs/WorkoutCatalog/WorkoutGridCard/assets/fatLoss.png";
+import muscleGainImage from "../../Catalogs/WorkoutCatalog/WorkoutGridCard/assets/muscleGain.png";
+
+
+
+const WorkoutProfileCard = ({ id, workoutTitle, difficulty }) => {
+  const image = difficulty === "Fat Loss" ? fatLossImage : muscleGainImage;
+
+  return (
+    <Link to={`/catalog/workouts/${id}`} className={styles.link}>
+      <div className={styles.workoutCard}>
+        <img src={image} alt="workout" />
+        <p>{workoutTitle}</p>
+      </div>
+    </Link>
+  );
+};
+
+WorkoutProfileCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  workoutTitle: PropTypes.string.isRequired,
+  difficulty: PropTypes.string.isRequired,
+};
+
+export default WorkoutProfileCard;
