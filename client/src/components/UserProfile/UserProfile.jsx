bad525c9c99b0b4ed8f728bdb53903ae2dd5ae9d
Georgi957 <gesh007@abv.bg>
2024-07-06 20:36:04 +0300

    Render own workouts in user profile

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,47 +1,66 @@
-import styles from './UserProfile.module.css'
+import styles from "./UserProfile.module.css";
 import { useUser } from "../../contexts/UserContext";
-import { useState, useEffect } from 'react';
-import { getUserRecipes } from '../../services/userService';
-import RecipeProfileCard from './RecipeProfileCard/RecipeProfileCard';
-import SecondaryLoader from '../Shared/SecondaryLoader/SecondaryLoader';
+import { useState, useEffect } from "react";
+import { getUserRecipes, getUserWorkouts } from "../../services/userService";
+import RecipeProfileCard from "./RecipeProfileCard/RecipeProfileCard";
+import SecondaryLoader from "../Shared/SecondaryLoader/SecondaryLoader";
+import WorkoutProfileCard from "./WorkoutProfileCard/WorkoutProfileCard";
 
 const UserProfile = () => {
-    const [recipes, setRecipes] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const { user } = useUser();
+  const [recipes, setRecipes] = useState([]);
+  const [workouts, setWorkouts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { user } = useUser();
 
-    useEffect(() => {
-        (async () => {
-            setLoading(true);
-            await getUserRecipes(user.userId, setRecipes);
-            setLoading(false);
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      await getUserRecipes(user.userId, setRecipes);
+      await getUserWorkouts(user.userId, setWorkouts);
+      setLoading(false);
+    })();
+  }, []);
 
-    if (loading) {
-        return <SecondaryLoader />
-    }
+  if (loading) {
+    return <SecondaryLoader />;
+  }
 
-    return (
-      <div className={styles.userProfileContainer}>
-        <div className={styles.greeting}>
-          <h2>Welcome, {user.username}!</h2>
-          <span>This is your user profile.</span>
-        </div>
-
-        <div className={styles.recipes}>
-          <h3>Your Recipes:</h3>
-          {recipes.length === 0 && (
-            <p className={styles.noRecipes}>You haven&apos;t created any recipes yet.</p>
-          )}
-          <ul className={styles.recipeList}>
-            {recipes.map((recipe) => (
-              <RecipeProfileCard key={recipe.id} recipeData={recipe} />
-            ))}
-          </ul>
-        </div>
+  return (
+    <div className={styles.userProfileContainer}>
+      <div className={styles.greeting}>
+        <h2>Welcome, {user.username}!</h2>
+        <span>This is your user profile.</span>
       </div>
-    );
+
+      <div className={styles.entity}>
+        <h3>Your Recipes:</h3>
+        {recipes.length === 0 && (
+          <p className={styles.noRecipes}>
+            You haven&apos;t created any recipes yet.
+          </p>
+        )}
+        <ul className={styles.recipeList}>
+          {recipes.map((recipe) => (
+            <RecipeProfileCard key={recipe.id} recipeData={recipe} />
+          ))}
+        </ul>
+      </div>
+
+      <div className={styles.entity}>
+        <h3>Your Workouts:</h3>
+        {workouts.length === 0 && (
+          <p className={styles.noRecipes}>
+            You haven&apos;t created any workouts yet.
+          </p>
+        )}
+        <ul className={styles.recipeList}>
+          {workouts.map((workout) => (
+            <WorkoutProfileCard key={workout.id} id={workout.id} workoutTitle={workout.title} workoutDifficulty={workout.difficulty} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
