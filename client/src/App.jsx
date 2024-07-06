79ad16eba984c11840eb87b545c6071a336b1ba6
Georgi957 <gesh007@abv.bg>
2024-07-06 15:57:49 +0300

    Created Workout details component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,25 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import Navigation from './components/Navigation/Navigation'
-import Home from './components/Home/Home'
-import Catalog from './components/Catalogs/CatalogHomeView/Catalog'
-import RecipeCatalog from './components/Catalogs/RecipeCatalog/RecipeCatalog'
-import WorkoutCatalog from './components/Catalogs/WorkoutCatalog/WorkoutCatalog'
-import Login from './components/Auth/Login'
-import Register from './components/Auth/Register'
-import Loader from './components/Shared/PrimaryLoader/Loader'
-import { useUser } from './contexts/UserContext'
-import CreateRecipe from './components/CreateForms/CreateRecipeForm/RecipeCreate'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Home from "./components/Home/Home";
+import Catalog from "./components/Catalogs/CatalogHomeView/Catalog";
+import RecipeCatalog from "./components/Catalogs/RecipeCatalog/RecipeCatalog";
+import WorkoutCatalog from "./components/Catalogs/WorkoutCatalog/WorkoutCatalog";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Loader from "./components/Shared/PrimaryLoader/Loader";
+import { useUser } from "./contexts/UserContext";
+import CreateRecipe from "./components/CreateForms/CreateRecipeForm/RecipeCreate";
 import PreCreateFormView from "./components/CreateForms/PreCreateFormView";
-import CreateWorkout from './components/CreateForms/CreateWorkoutForm/WorkoutCreate'
-import RecipeDetails from './components/Details/RecipeDetails/RecipeDetails'
-import EditRecipe from './components/EditForms/EditRecipe'
-import UserProfile from './components/UserProfile/UserProfile'
+import CreateWorkout from "./components/CreateForms/CreateWorkoutForm/WorkoutCreate";
+import RecipeDetails from "./components/Details/RecipeDetails/RecipeDetails";
+import EditRecipe from "./components/EditForms/EditRecipe";
+import UserProfile from "./components/UserProfile/UserProfile";
 
 function App() {
   const { loading } = useUser();
-  
-  if(loading) return <Loader />
+
+  if (loading) return <Loader />;
 
   return (
     <>
@@ -33,12 +33,13 @@ function App() {
             <Route path="catalog/recipes/:id" element={<RecipeDetails />} />
             <Route path="catalog/recipes/:id/edit" element={<EditRecipe />} />
             <Route path="catalog/workouts" element={<WorkoutCatalog />} />
+            <Route path="catalog/workouts/:id" element={<RecipeDetails />} />
             <Route path="create" element={<PreCreateFormView />} />
             <Route path="create/recipe" element={<CreateRecipe />} />
             <Route path="create/workout" element={<CreateWorkout />} />
             <Route path="users/login" element={<Login />} />
             <Route path="users/register" element={<Register />} />
-            <Route path="users/profile" element={<UserProfile/>} />
+            <Route path="users/profile" element={<UserProfile />} />
           </Routes>
         </div>
       </div>
---
+++ client/src/components/Details/WorkoutDetails/WorkoutDetails.jsx
@@ -0,0 +1,9 @@
+import styles from "./WorkoutDetails.module.css"
+
+const WorkoutDetails = () => {
+    return (
+        <h2>Workout Details</h2>
+    );
+};
+
+export default WorkoutDetails;
