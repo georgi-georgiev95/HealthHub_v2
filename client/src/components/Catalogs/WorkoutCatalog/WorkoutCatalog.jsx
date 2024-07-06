b2e8a08592938949e1f6021abd061fcfca44d15c
Georgi957 <gesh007@abv.bg>
2024-07-06 15:55:06 +0300

    Render grid card components and show loader

--- client/src/components/Catalogs/WorkoutCatalog/WorkoutCatalog.jsx
+++ client/src/components/Catalogs/WorkoutCatalog/WorkoutCatalog.jsx
@@ -1,18 +1,26 @@
 import styles from "./WorkoutCatalog.module.css"
 import { useEffect, useState } from "react";
-import {getAllWorkouts} from "../../../services/workoutService"
+import { getAllWorkouts } from "../../../services/workoutService"
+import WorkoutGridCard from "./WorkoutGridCard/WorkoutGridCard";
+import SecondaryLoader from "../../Shared/SecondaryLoader/SecondaryLoader";
 
 const WorkoutCatalog = () => {
-    const [workouts, setWorkouts] = useState([]);
+  const [workouts, setWorkouts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
-            getAllWorkouts(setWorkouts);
+          getAllWorkouts(setWorkouts);
+          setLoading(false);
         })();
     }, []);
-
-    console.log(workouts);
+  
+  console.log(workouts);
     
+    if (loading) {
+      return <SecondaryLoader />;
+    }
+  
     return (
       <>
         <div className={styles.title}>
@@ -23,8 +31,20 @@ const WorkoutCatalog = () => {
         </div>
         <div className={styles.container}>
           {workouts.length === 0 && (
-            <p className={styles.noRecipes}>No workouts found.</p>
+            <p className={styles.noWorkouts}>No workouts found.</p>
           )}
+          {workouts.length > 0 && 
+            workouts.map((workout) => (
+              <WorkoutGridCard
+                key={workout.id}
+                title={workout.title}
+                difficulty={workout.difficulty}
+                goal={workout.goal}
+                ownerName={workout.ownerName}
+                id={workout.id}
+              />
+            ))
+          }
         </div>
       </>
     );
