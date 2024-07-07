6dc5dbb376d0320d07d433e61f429b108926712d
Georgi957 <gesh007@abv.bg>
2024-07-07 16:56:59 +0300

    Rearrange profile page

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,7 +1,12 @@
 import styles from "./UserProfile.module.css";
 import { useUser } from "../../contexts/UserContext";
 import { useState, useEffect } from "react";
-import { getUserRecipes, getUserWorkouts, getUserLikedRecipes, getUserLikedWorkouts } from "../../services/userService";
+import {
+  getUserRecipes,
+  getUserWorkouts,
+  getUserLikedRecipes,
+  getUserLikedWorkouts,
+} from "../../services/userService";
 import RecipeProfileCard from "./RecipeProfileCard/RecipeProfileCard";
 import SecondaryLoader from "../Shared/SecondaryLoader/SecondaryLoader";
 import WorkoutProfileCard from "./WorkoutProfileCard/WorkoutProfileCard";
@@ -25,8 +30,6 @@ const UserProfile = () => {
     })();
   }, []);
 
-  console.log(likedRecipes);
-  console.log(likedWorkouts);
 
   if (loading) {
     return <SecondaryLoader />;
@@ -39,70 +42,76 @@ const UserProfile = () => {
         <span>This is your user profile.</span>
       </div>
 
-      <div className={styles.entity}>
-        <h3>Your Recipes:</h3>
-        {recipes.length === 0 && (
-          <p className={styles.noRecipes}>
-            You haven&apos;t created any recipes yet.
-          </p>
-        )}
-        <ul className={styles.recipeList}>
-          {recipes.map((recipe) => (
-            <RecipeProfileCard key={recipe.id} recipeData={recipe} />
-          ))}
-        </ul>
-      </div>
+      <div className={styles.container}>
+        <div className={styles.created}>
+          <div className={styles.entity}>
+            <h3>Your Recipes:</h3>
+            {recipes.length === 0 && (
+              <p className={styles.noRecipes}>
+                You haven&apos;t created any recipes yet.
+              </p>
+            )}
+            <ul className={styles.recipeList}>
+              {recipes.map((recipe) => (
+                <RecipeProfileCard key={recipe.id} recipeData={recipe} />
+              ))}
+            </ul>
+          </div>
 
-      <div className={styles.entity}>
-        <h3>Your Workouts:</h3>
-        {workouts.length === 0 && (
-          <p className={styles.noRecipes}>
-            You haven&apos;t created any workouts yet.
-          </p>
-        )}
-        <ul className={styles.recipeList}>
-          {workouts.map((workout) => (
-            <WorkoutProfileCard
-              key={workout.id}
-              id={workout.id}
-              workoutTitle={workout.title}
-              workoutGoal={workout.goal}
-            />
-          ))}
-        </ul>
-      </div>
+          <div className={styles.entity}>
+            <h3>Your Workouts:</h3>
+            {workouts.length === 0 && (
+              <p className={styles.noRecipes}>
+                You haven&apos;t created any workouts yet.
+              </p>
+            )}
+            <ul className={styles.recipeList}>
+              {workouts.map((workout) => (
+                <WorkoutProfileCard
+                  key={workout.id}
+                  id={workout.id}
+                  workoutTitle={workout.title}
+                  workoutGoal={workout.goal}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
 
-      <div className={styles.entity}>
-        <h3>Your Liked Recipes:</h3>
-        {likedRecipes.length === 0 && (
-          <p className={styles.noRecipes}>
-            You haven&apos;t liked any recipes yet.
-          </p>
-        )}
-        <ul className={styles.recipeList}>
-          {likedRecipes.map((recipe) => (
-            <RecipeProfileCard key={recipe.id} recipeData={recipe} />
-          ))}
-        </ul>
-      </div>
+        <div className={styles.liked}>
+          <div className={styles.entity}>
+            <h3>Your Liked Recipes:</h3>
+            {likedRecipes.length === 0 && (
+              <p className={styles.noRecipes}>
+                You haven&apos;t liked any recipes yet.
+              </p>
+            )}
+            <ul className={styles.recipeList}>
+              {likedRecipes.map((recipe) => (
+                <RecipeProfileCard key={recipe.id} recipeData={recipe} />
+              ))}
+            </ul>
+          </div>
 
-      <div className={styles.entity}>
-        <h3>Your Liked Workouts:</h3>
-        {likedWorkouts.length === 0 && (
-          <p className={styles.noRecipes}>
-            You haven&apos;t liked any workouts yet.
-          </p>
-        )}
-        <ul className={styles.recipeList}>
-          {likedWorkouts.map((workout) => (
-            <WorkoutProfileCard
-              key={workout.id}
-              id={workout.id}
-              workoutTitle={workout.title}
-              workoutGoal={workout.goal}
-            />
-          ))}
-        </ul>
+          <div className={styles.entity}>
+            <h3>Your Liked Workouts:</h3>
+            {likedWorkouts.length === 0 && (
+              <p className={styles.noRecipes}>
+                You haven&apos;t liked any workouts yet.
+              </p>
+            )}
+            <ul className={styles.recipeList}>
+              {likedWorkouts.map((workout) => (
+                <WorkoutProfileCard
+                  key={workout.id}
+                  id={workout.id}
+                  workoutTitle={workout.title}
+                  workoutGoal={workout.goal}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
