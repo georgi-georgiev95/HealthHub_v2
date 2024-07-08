882475d5b77eeea2daddeb171391fd4cf8672015
Georgi957 <gesh007@abv.bg>
2024-07-08 10:51:15 +0300

    Replaced repeating code with the extracted components

--- client/src/components/Catalogs/CatalogHomeView/Catalog.jsx
+++ client/src/components/Catalogs/CatalogHomeView/Catalog.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./Catalog.module.css";
+import { LargeRecipesLink } from "../../Shared/LargeLinks/LargeRecipesLink";
+import { LargeWorkoutsLink } from "../../Shared/LargeLinks/LargeWorkoutsLink";
 
 const Catalog = () => {
   return (
@@ -7,34 +9,14 @@ const Catalog = () => {
       <h2 className={styles.catalogIntro}>Explore our catalog</h2>
       <div className={styles.createHomeView}>
         <Link className={styles.groupLink} to="/catalog/recipes">
-          <div className={styles.groupLink}>
-            <div className={styles.upperSide}>
-              <img
-                src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505"
-                alt=""
-              />
-            </div>
-            <div className={styles.bottomSide}>
-              <h3>Recipes</h3>
-            </div>
-          </div>
+          <LargeRecipesLink />
         </Link>
         <div className={styles.logo}></div>
         <Link
           className={`${styles.groupLink} ${styles.reverse}`}
           to="/catalog/workouts"
         >
-          <div className={`${styles.groupLink} ${styles.reverse}`}>
-            <div className={styles.bottomSide}>
-              <img
-                src="https://media.istockphoto.com/id/1391410249/photo/sports-and-gym-activities.jpg?s=612x612&w=0&k=20&c=1S-hAmT-CkRtdYV_hcKi1lZdQkXAN_mCy3ebIXlUEnE="
-                alt=""
-              />
-            </div>
-            <div className={styles.upperSide}>
-              <h3>Workouts</h3>
-            </div>
-          </div>
+          <LargeWorkoutsLink />
         </Link>
       </div>
     </>
