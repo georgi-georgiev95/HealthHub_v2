a9de4762967d251ea6911a839b0cd3330cef28d5
Georgi957 <gesh007@abv.bg>
2024-07-09 17:15:54 +0300

    Created route guard for user routes

---
+++ client/src/utils/UserRoutesProtector.jsx
@@ -0,0 +1,14 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
+
+const UserRoutesProtector = () => {
+    const { user } = useUser();
+
+    if (user.userId === null || user.userId === undefined || user.userId === "") {
+        return <Navigate to="/users/login" />;
+    }
+
+    return <Outlet />
+};
+
+export default UserRoutesProtector;
