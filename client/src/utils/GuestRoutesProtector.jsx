cd06bede6485c6a5142cb151a838013392af8fbe
Georgi957 <gesh007@abv.bg>
2024-07-09 17:16:12 +0300

    Created route guard for guest routes

---
+++ client/src/utils/GuestRoutesProtector.jsx
@@ -0,0 +1,14 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
+
+const GuestRoutesProtector = () => {
+  const { user } = useUser();
+
+  if (user.userId !== null && user.userId !== undefined && user.userId !== "") {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
+};
+
+export default GuestRoutesProtector;
