79037a9c65134c08d1b6e54bd4734a5b6c074026
Georgi957 <gesh007@abv.bg>
2024-07-14 12:19:24 +0300

    Added lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,29 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
-import Catalog from "./components/Catalogs/CatalogHomeView/Catalog";
-import RecipeCatalog from "./components/Catalogs/RecipeCatalog/RecipeCatalog";
-import WorkoutCatalog from "./components/Catalogs/WorkoutCatalog/WorkoutCatalog";
-import Login from "./components/Auth/Login";
-import Register from "./components/Auth/Register";
 import Loader from "./components/Shared/PrimaryLoader/Loader";
 import { useUser } from "./contexts/UserContext";
-import CreateRecipe from "./components/CreateForms/CreateRecipeForm/RecipeCreate";
-import PreCreateFormView from "./components/CreateForms/PreCreateFormView";
-import CreateWorkout from "./components/CreateForms/CreateWorkoutForm/WorkoutCreate";
-import RecipeDetails from "./components/Details/RecipeDetails/RecipeDetails";
-import EditRecipe from "./components/EditForms/EditRecipe";
-import UserProfile from "./components/UserProfile/UserProfile";
-import WorkoutDetails from "./components/Details/WorkoutDetails/WorkoutDetails";
-import EditWorkout from "./components/EditForms/EditWorkout";
 import PageNotFound from "./components/BadRoutes/PageNotFound";
 import UnauthorizedRequest from "./components/BadRoutes/UnauthorizedRequest";
 import UserRoutesProtector from "./components/Utils/UserRoutesProtector";
 import GuestRoutesProtector from "./components/Utils/GuestRoutesProtector";
 import OwnerRoutesProtector from "./components/Utils/OwnerRoutesProtector";
+
+const Catalog = lazy(() => import("./components/Catalogs/CatalogHomeView/Catalog"));
+const RecipeCatalog = lazy(() => import("./components/Catalogs/RecipeCatalog/RecipeCatalog"));
+const WorkoutCatalog = lazy(() => import("./components/Catalogs/WorkoutCatalog/WorkoutCatalog"));
+const RecipeDetails = lazy(() => import("./components/Details/RecipeDetails/RecipeDetails"));
+const WorkoutDetails = lazy(() => import("./components/Details/WorkoutDetails/WorkoutDetails"));
+const EditRecipe = lazy(() => import("./components/EditForms/EditRecipe"));
+const EditWorkout = lazy(() => import("./components/EditForms/EditWorkout"));
+const PreCreateFormView = lazy(() => import("./components/CreateForms/PreCreateFormView"));
+const CreateRecipe = lazy(() => import("./components/CreateForms/CreateRecipeForm/RecipeCreate"));
+const CreateWorkout = lazy(() => import("./components/CreateForms/CreateWorkoutForm/WorkoutCreate"));
+const UserProfile = lazy(() => import("./components/UserProfile/UserProfile"));
+const Login = lazy(() => import("./components/Auth/Login"));
+const Register = lazy(() => import("./components/Auth/Register"));
 
 function App() {
   const { loading } = useUser();
@@ -33,35 +35,42 @@ function App() {
       <Navigation />
       <div className="site">
         <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="catalog" element={<Catalog />} />
-            <Route path="catalog/recipes" element={<RecipeCatalog />} />
-            <Route path="catalog/recipes/:id" element={<RecipeDetails />} />
-            <Route path="catalog/workouts" element={<WorkoutCatalog />} />
-            <Route path="catalog/workouts/:id" element={<WorkoutDetails />} />
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="catalog" element={<Catalog />} />
+              <Route path="catalog/recipes" element={<RecipeCatalog />} />
+              <Route path="catalog/recipes/:id" element={<RecipeDetails />} />
+              <Route path="catalog/workouts" element={<WorkoutCatalog />} />
+              <Route path="catalog/workouts/:id" element={<WorkoutDetails />} />
 
-            <Route element={<OwnerRoutesProtector />}>
-              <Route path="catalog/recipes/:id/edit" element={<EditRecipe />} />
-              <Route path="catalog/workouts/:id/edit" element={<EditWorkout />}
-              />
-            </Route>
+              <Route element={<OwnerRoutesProtector />}>
+                <Route
+                  path="catalog/recipes/:id/edit"
+                  element={<EditRecipe />}
+                />
+                <Route
+                  path="catalog/workouts/:id/edit"
+                  element={<EditWorkout />}
+                />
+              </Route>
 
-            <Route element={<UserRoutesProtector />}>
-              <Route path="create" element={<PreCreateFormView />} />
-              <Route path="create/recipe" element={<CreateRecipe />} />
-              <Route path="create/workout" element={<CreateWorkout />} />
-              <Route path="users/profile" element={<UserProfile />} />
-            </Route>
+              <Route element={<UserRoutesProtector />}>
+                <Route path="create" element={<PreCreateFormView />} />
+                <Route path="create/recipe" element={<CreateRecipe />} />
+                <Route path="create/workout" element={<CreateWorkout />} />
+                <Route path="users/profile" element={<UserProfile />} />
+              </Route>
 
-            <Route element={<GuestRoutesProtector />}>
-              <Route path="users/login" element={<Login />} />
-              <Route path="users/register" element={<Register />} />
-            </Route>
+              <Route element={<GuestRoutesProtector />}>
+                <Route path="users/login" element={<Login />} />
+                <Route path="users/register" element={<Register />} />
+              </Route>
 
-            <Route path="400" element={<UnauthorizedRequest />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+              <Route path="400" element={<UnauthorizedRequest />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </div>
       </div>
     </>
