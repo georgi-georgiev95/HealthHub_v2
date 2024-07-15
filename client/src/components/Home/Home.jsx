850d28255f899224beb6a1afed05170ca168d465
Georgi Georgiev <122177357+georgi-georgiev95@users.noreply.github.com>
2024-07-15 13:22:27 +0300

    Extracted home features in separate component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,8 +3,12 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Home.module.css";
+import HomeFeature from "./HomeFeature/HomeFeature";
 
 const Home = () => {
+  const recipesFeatureURL = 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505';
+  const workoutsFeatureURL = 'https://media.istockphoto.com/id/1391410249/photo/sports-and-gym-activities.jpg?s=612x612&w=0&k=20&c=1S-hAmT-CkRtdYV_hcKi1lZdQkXAN_mCy3ebIXlUEnE=';
+
   const catalogIntroRef = useRef(null);
 
   const handleScrollToCatalog = () => {
@@ -23,32 +27,24 @@ const Home = () => {
       </section>
 
       <section className={styles.features}>
-        <div className={styles.feature}>
-          <img
-            src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505"
-            alt="Create Recipe"
-          />
-          <h2>Create a Recipe</h2>
-          <p>Craft and share your unique recipes with the community.</p>
-          <Link to="/create/recipe" className={styles.featureButton}>
-            Start Now
-          </Link>
-        </div>
+        <HomeFeature
+          imageUrl={recipesFeatureURL}
+          destination={'/create/recipe'}
+          title={'Create a Workout'}
+          message={'Design and share workout routines tailored to your needs.'}
+        />
+
         <div className={styles.arrow} onClick={handleScrollToCatalog}>
           <p>Check out what other users have created</p>
           <FontAwesomeIcon icon={faChevronDown} />
         </div>
-        <div className={styles.feature}>
-          <img
-            src="https://media.istockphoto.com/id/1391410249/photo/sports-and-gym-activities.jpg?s=612x612&w=0&k=20&c=1S-hAmT-CkRtdYV_hcKi1lZdQkXAN_mCy3ebIXlUEnE="
-            alt="Create Workout"
-          />
-          <h2>Create a Workout</h2>
-          <p>Design and share workout routines tailored to your needs.</p>
-          <Link to="/create/workout" className={styles.featureButton}>
-            Start Now
-          </Link>
-        </div>
+
+        <HomeFeature
+          imageUrl={workoutsFeatureURL}
+          destination={'/create/workout'}
+          title={'Create a Workout'}
+          message={'Design and share workout routines tailored to your needs.'}
+        />
       </section>
 
       <section ref={catalogIntroRef} className={styles.catalogIntro}>
