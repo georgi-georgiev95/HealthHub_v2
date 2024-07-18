b8bf93686bb5743de9a268b20f670368c19d2f0a
Georgi Georgiev <122177357+georgi-georgiev95@users.noreply.github.com>
2024-07-18 11:32:46 +0300

    Mock geolocation to test App component

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,18 +1,34 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
+import { vi } from 'vitest';
 
 import UserProvider from './contexts/UserProvider'
 import App from './App'
 
+beforeAll(() => {
+    global.navigator.geolocation = {
+        getCurrentPosition: vi.fn().mockImplementation((success, error) => {
+            success({
+                coords: {
+                    latitude: 51.1,
+                    longitude: 45.3
+                }
+            });
+        })
+    };
+});
+
 describe('App', () => {
-    it('renders the App component', () => {
-        render(
+    it('renders the App component', async () => {
+        await act(async () => {
+            render(
             <UserProvider>
                 <BrowserRouter>
                     <App />
                 </BrowserRouter>
             </UserProvider>
-        )
+        );
+    });
 
         screen.debug(); // prints out the jsx in the App component unto the command line
     })
