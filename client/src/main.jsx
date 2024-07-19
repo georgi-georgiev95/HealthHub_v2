7de049ab89a0b2f160f3eefae25a3c8d3c1cba23
Georgi957 <gesh007@abv.bg>
2024-07-19 20:36:19 +0300

    Enable react strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,11 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <UserProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </UserProvider>
+  </React.StrictMode>
 );
