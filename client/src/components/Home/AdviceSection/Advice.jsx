ea2648c7f26643e4b4f2abdb3757d04d41825cb4
Georgi957 <gesh007@abv.bg>
2024-07-21 19:50:39 +0300

    Fix clear interval

--- client/src/components/Home/AdviceSection/Advice.jsx
+++ client/src/components/Home/AdviceSection/Advice.jsx
@@ -8,14 +8,14 @@ const AdviceComponent = () => {
     });
 
     useEffect(() => {
-        setInterval(async () => {
+        const interval = setInterval(async () => {
             const response = await fetch('https://api.adviceslip.com/advice');
             const data = await response.json();
             setAdvice(data.slip);
         }, 5000);
 
         return () => {
-            clearInterval();
+            clearInterval(interval);
         }
     }, []);
 
