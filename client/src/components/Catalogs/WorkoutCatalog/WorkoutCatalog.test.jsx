b6c73f4a3f20a9fc06056dd4c79a41d515f42d04
Georgi957 <gesh007@abv.bg>
2024-07-25 15:14:46 +0300

    Add test for component

---
+++ client/src/components/Catalogs/WorkoutCatalog/WorkoutCatalog.test.jsx
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { BrowserRouter } from "react-router-dom";
+
+import WorkoutCatalog from "./WorkoutCatalog";
+import UserProvider from "../../../contexts/userContext/UserProvider";
+import CommentsProvider from "../../../contexts/commentsContext/CommentsProvired";
+import useFetch from "../../../hooks/useFetch";
+
+vi.mock("../../../hooks/useFetch");
+
+const renderWorkoutCatalog = () => {
+  render(
+    <UserProvider>
+      <CommentsProvider>
+        <BrowserRouter>
+          <WorkoutCatalog />
+        </BrowserRouter>
+      </CommentsProvider>
+    </UserProvider>
+  );
+};
+
+describe("WorkoutCatalog Component", () => {
+  it("renders the workouts correctly", () => {
+    useFetch.mockReturnValue({
+      data: [
+        {
+          id: 1,
+          title: "Workout 1",
+          difficulty: "Easy",
+          goal: "Strength",
+          ownerName: "John Doe",
+        },
+        {
+          id: 2,
+          title: "Workout 2",
+          difficulty: "Medium",
+          goal: "Cardio",
+          ownerName: "Jane Smith",
+        },
+      ],
+      loading: false,
+    });
+    renderWorkoutCatalog();
+    expect(screen.getByText("Workouts")).toBeInTheDocument();
+    expect(screen.getByText("From rookie to expert!")).toBeInTheDocument();
+    expect(screen.getByText("Workout 1")).toBeInTheDocument();
+    expect(screen.getByText("Workout 2")).toBeInTheDocument();
+  });
+
+  it("renders no workouts message correctly", () => {
+    useFetch.mockReturnValue({ data: [], loading: false });
+    renderWorkoutCatalog();
+    expect(screen.getByText("No workouts found.")).toBeInTheDocument();
+  });
+});
