274756003e314c55464fc3c3fc20719164638fcc
Georgi957 <gesh007@abv.bg>
2024-07-25 15:01:42 +0300

    Add test for WorkoutGridCard component

---
+++ client/src/components/Catalogs/WorkoutCatalog/WorkoutGridCard/WorkoutGridCard.test.jsx
@@ -0,0 +1,50 @@
+const { render, screen } = require("@testing-library/react");
+import { BrowserRouter } from "react-router-dom";
+
+import WorkoutGridCard from "./WorkoutGridCard";
+import UserProvider from "../../../../contexts/userContext/UserProvider";
+
+const renderWorkoutGridCard = () => {
+  render(
+    <UserProvider>
+      <BrowserRouter>
+        <WorkoutGridCard
+          title="Workout Title"
+          difficulty="⭐"
+          goal="Weight Loss"
+          ownerName="Owner Name"
+          id="1"
+        />
+      </BrowserRouter>
+    </UserProvider>
+  );
+};
+
+describe("WorkoutGridCard Component", () => {
+  it("renders correctly", () => {
+    renderWorkoutGridCard();
+    expect(screen.getByText("Workout Title")).toBeInTheDocument();
+    expect(screen.getByText("Difficulty: ⭐")).toBeInTheDocument();
+    expect(screen.getByText("Goal: Weight Loss")).toBeInTheDocument();
+    expect(screen.getByText("Added by: Owner Name")).toBeInTheDocument();
+  });
+
+  it("renders image", () => {
+    renderWorkoutGridCard();
+    expect(screen.getByAltText("Workout")).toBeInTheDocument();
+  });
+
+  it("renders link", () => {
+    renderWorkoutGridCard();
+    expect(screen.getByRole("link")).toBeInTheDocument();
+  });
+
+  it("link navigates to correct page", () => {
+    renderWorkoutGridCard();
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      "/catalog/workouts/1"
+    );
+  });
+    
+});
