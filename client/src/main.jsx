7d0dbdad508d106eaae1925eb253dbc2dba7fb3d
Georgi957 <gesh007@abv.bg>
2024-07-28 21:04:01 +0300

    Remove comments provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,17 +5,14 @@ import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import "./index.css";
-import CommentsProvider from "./contexts/commentsContext/CommentsProvired.jsx";
 import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <CommentsProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </CommentsProvider>
     </Provider>
   </React.StrictMode>
 );
