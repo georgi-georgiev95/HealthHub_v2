e774a40d015a51dc6c3a94fd2dd4c972907a4511
Georgi957 <gesh007@abv.bg>
2024-07-28 21:03:54 +0300

    Add comments reducer

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import commentsReducer from './slices/commentsSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        comments: commentsReducer,
     }
 });
 
