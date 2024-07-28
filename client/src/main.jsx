60fa06f8f8151df0983fbdc313d60cd3263da345
Georgi957 <gesh007@abv.bg>
2024-07-28 20:20:33 +0300

    Replace UserProvider with redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-
+import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import "./index.css";
-import UserProvider from "./contexts/userContext/UserProvider.jsx";
 import CommentsProvider from "./contexts/commentsContext/CommentsProvired.jsx";
+import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserProvider>
+    <Provider store={store}>
       <CommentsProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </CommentsProvider>
-    </UserProvider>
+    </Provider>
   </React.StrictMode>
 );
