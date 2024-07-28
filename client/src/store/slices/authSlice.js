e1c78ec7de9933cab1a6ac930187174d471153c8
Georgi957 <gesh007@abv.bg>
2024-07-28 20:20:50 +0300

    Create auth slice

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -1,7 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { firebaseAuth } from '../../config/firebase';
+
+export const verifyUserSession = createAsyncThunk('auth/verifyUserSession', async (_, { rejectWithValue }) => {
+    return new Promise((resolve, reject) => {
+        firebaseAuth.onAuthStateChanged(user => {
+            if (user) {
+                const userData = {
+                    userId: user.uid,
+                    email: user.email,
+                    username: user.displayName,
+                };
+                resolve(userData);
+            } else {
+                resolve(null);
+            }
+        });
+    });
+});
 
 const initialState = {
     user: null,
+    loading: false,
 };
 
 const authSlice = createSlice({
@@ -18,8 +37,23 @@ const authSlice = createSlice({
             state.user = null;
         },
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(verifyUserSession.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(verifyUserSession.fulfilled, (state, action) => {
+                state.user = action.payload;
+                state.loading = false;
+            })
+            .addCase(verifyUserSession.rejected, (state) => {
+                state.loading = false;
+            });
+    },
 });
 
 export const { login, register, logout } = authSlice.actions;
+
+export const selectLoading = (state) => state.auth.loading;
 
 export default authSlice.reducer;
