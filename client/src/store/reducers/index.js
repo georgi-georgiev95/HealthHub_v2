722bfee9a83da8d996000e4ae91fa40529ab97fa
Georgi957 <gesh007@abv.bg>
2024-07-28 18:35:59 +0300

    Use auth reducer in root reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
-    //
+    authenticator: authReducer,
 });
 
 export default rootReducer;
