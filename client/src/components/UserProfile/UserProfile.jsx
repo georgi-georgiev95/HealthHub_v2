26c39634e177106df5809438d7f0d4ef786421f8
Georgi957 <gesh007@abv.bg>
2024-07-29 14:57:54 +0300

    Use custom hook for user profile

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,39 +1,12 @@
-import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-
 import styles from "./UserProfile.module.css";
-import {
-  getUserRecipes,
-  getUserWorkouts,
-  getUserLikedRecipes,
-  getUserLikedWorkouts,
-} from "../../services/userService";
+import useFetchUserProfileData from "../../hooks/useFetchUserProfileData";
 import SecondaryLoader from "../Shared/SecondaryLoader/SecondaryLoader";
 import CreatedSection from "./CreatedSection/CreatedSection";
 import LikedSection from "./LikedSection/LikedSection";
 
 const UserProfile = () => {
-  const [recipes, setRecipes] = useState([]);
-  const [workouts, setWorkouts] = useState([]);
-  const [likedRecipes, setLikedRecipes] = useState([]);
-  const [likedWorkouts, setLikedWorkouts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const user = useSelector((state) => state.auth.user);
-
-  useEffect(() => {
-    (async () => {
-      setLoading(true);
-      const recipes = await getUserRecipes(user.userId);
-      const workouts = await getUserWorkouts(user.userId);
-      const likedRecipes = await getUserLikedRecipes(user.userId);
-      const likedWorkouts = await getUserLikedWorkouts(user.userId);
-      setRecipes(recipes);
-      setWorkouts(workouts);
-      setLikedRecipes(likedRecipes);
-      setLikedWorkouts(likedWorkouts);
-      setLoading(false);
-    })();
-  }, []);
+  const { recipes, workouts, likedRecipes, likedWorkouts, loading, user } =
+    useFetchUserProfileData();
 
   if (loading) {
     return <SecondaryLoader />;
